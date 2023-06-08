477bfa5fe98e9058f29c33752d2703c06e3d82cd
yjaal <yjjal1314@gmail.com>
2023-06-08 11:34:23 +0800

    自定义表单

--- src/main/java/win/iot4yj/config/SecurityConf.java
+++ src/main/java/win/iot4yj/config/SecurityConf.java
@@ -1,8 +1,13 @@
 package win.iot4yj.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * security 配置
@@ -13,14 +18,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConf extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.authorizeRequests()
             .anyRequest()
             .authenticated()
             .and()
             .formLogin()
+            // 指定登陆页面
+            .loginPage("/login.html")
+            .loginProcessingUrl("/doLogin")
+            .defaultSuccessUrl("/index")
+            .failureUrl("/login.html")
+            .usernameParameter("name")
+            .passwordParameter("pass")
+            .permitAll()
             .and()
-            .httpBasic();
+            .csrf().disable();
+    }
+
+    /**
+     * 对相关静态资源放行
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("joy")
+            .password("walp1314")
+            .roles("admin");
     }
 }
--- src/main/java/win/iot4yj/controller/UserController.java
+++ src/main/java/win/iot4yj/controller/UserController.java
@@ -1,18 +1,20 @@
 package win.iot4yj.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * todo
  *
  * @author YJ
  * @date 2023/6/6
  **/
 @RestController
-@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "login success";
+    }
 
     @GetMapping("/hello")
     public String hello() {
